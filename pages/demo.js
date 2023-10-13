226303013a9d9e0136874c2512222266c2f5b386
cavj83 <l23210559@tectijuana.edu.mx>
2023-10-13 02:48:28 -0700

    update demo 	modified:   pages/demo.js

--- pages/demo.js
+++ pages/demo.js
@@ -1,7 +1,361 @@
-export default function Demo() {
+import toast, { Toaster } from "react-hot-toast";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import {
+    Connection,
+    SystemProgram,
+    Transaction,
+    PublicKey,
+    LAMPORTS_PER_SOL,
+    clusterApiUrl,
+    SendTransactionError,
+} from "@solana/web3.js";
+import { useStorageUpload } from "@thirdweb-dev/react";
+
+import axios from "axios";
+import nom from "img.json";
+
+const img = nom.img;
+const SOLANA_NETWORK = "devnet";
+
+const Home = () => {
+    const [publicKey, setPublicKey] = useState(null);
+    const router = useRouter();
+    const [balance, setBalance] = useState(0);
+    const [receiver, setReceiver] = useState(null);
+    const [amount, setAmount] = useState(null);
+    const [explorerLink, setExplorerLink] = useState(null);
+
+    const [uploadUrl, setUploadUrl] = useState(null);
+    const [url, setUrl] = useState(null);
+    const [statusText, setStatusText] = useState("");
+    const [nomid,setnomid]=useState(null);
+
+    useEffect(() => {
+        let key = window.localStorage.getItem("publicKey"); //obtiene la publicKey del localStorage
+        setPublicKey(key);
+        if (key) getBalances(key);
+        if (explorerLink) setExplorerLink(null);
+    }, []);
+
+    const handleReceiverChange = (event) => {
+        setReceiver(event.target.value);
+    };
+
+    const handleAmountChange = (event) => {
+        setAmount(event.target.value);
+    };
+
+    const handleSubmit = async () => {
+        console.log("Este es el receptor", receiver);
+        console.log("Este es el monto", amount);
+        sendTransaction();
+    };
+
+    const handleUrlChange = (event) => {
+        setUrl(event.target.value);
+        console.log("Si se esta seteando la URL", url);
+    };
+
+    //Funcion para Iniciar sesion con nuestra Wallet de Phantom
+
+    const signIn = async () => {
+        //Si phantom no esta instalado
+        const provider = window?.phantom?.solana;
+        const { solana } = window;
+
+        if (!provider?.isPhantom || !solana.isPhantom) {
+            toast.error("Phantom no esta instalado");
+            setTimeout(() => {
+                window.open("https://phantom.app/", "_blank");
+            }, 2000);
+            return;
+        }
+        //Si phantom esta instalado
+        let phantom;
+        if (provider?.isPhantom) phantom = provider;
+
+        const { publicKey } = await phantom.connect(); //conecta a phantom
+        console.log("publicKey", publicKey.toString()); //muestra la publicKey
+        setPublicKey(publicKey.toString()); //guarda la publicKey en el state
+        window.localStorage.setItem("publicKey", publicKey.toString()); //guarda la publicKey en el localStorage
+
+        toast.success("Tu Nomina esta conectada 💵");
+
+        getBalances(publicKey);
+    };
+
+    //Funcion para cerrar sesion con nuestra Wallet de Phantom
+
+    const signOut = async () => {
+        if (window) {
+            const { solana } = window;
+            window.localStorage.removeItem("publicKey");
+            setPublicKey(null);
+            solana.disconnect();
+            router.reload(window?.location?.pathname);
+        }
+    };
+
+    //Funcion para obtener el balance de nuestra wallet
+
+    const getBalances = async (publicKey) => {
+        try {
+            const connection = new Connection(
+                clusterApiUrl(SOLANA_NETWORK),
+                "confirmed"
+            );
+
+            const balance = await connection.getBalance(
+                new PublicKey(publicKey)
+            );
+
+            const balancenew = balance / LAMPORTS_PER_SOL;
+            setBalance(balancenew);
+        } catch (error) {
+            console.error("ERROR GET BALANCE", error);
+            toast.error("Something went wrong getting the balance");
+        }
+    };
+
+    //Funcion para enviar una transaccion
+    const sendTransaction = async () => {
+        try {
+            //Consultar el balance de la wallet
+            getBalances(publicKey);
+            console.log("Este es el balance", balance);
+
+            //Si el balance es menor al monto a enviar
+            if (balance < amount) {
+                toast.error("No tienes suficiente balance");
+                return;
+            }
+
+            const provider = window?.phantom?.solana;
+            const connection = new Connection(
+                clusterApiUrl(SOLANA_NETWORK),
+                "confirmed"
+            );
+
+            //Llaves
+            //CTA empresa
+            const fromPubkey = new PublicKey("7YEpVLoh4KHmNPygRAhW4HeG1XjxdyJsQPbM9ui7bjMS");//(publicKey);
+            //CTA PAC de Timbrado de Nomina
+            const toPubkey = new PublicKey("J39RGnXNF3pgkDj1MWQevJ6uQu483xp3w7BBpnKWwaSX");//(receiver);
+
+            //Creamos la transaccion
+            const transaction = new Transaction().add(
+                SystemProgram.transfer({
+                    fromPubkey,
+                    toPubkey,
+                    lamports: 0.0001 * LAMPORTS_PER_SOL,//amount * LAMPORTS_PER_SOL,
+                })
+            );
+            console.log("Esta es la transaccion", transaction);
+
+            //Traemos el ultimo blocke de hash
+            const { blockhash } = await connection.getLatestBlockhash();
+            transaction.recentBlockhash = blockhash;
+            transaction.feePayer = fromPubkey;
+
+            //Firmamos la transaccion
+            const transactionsignature = await provider.signTransaction(
+                transaction
+            );
+
+            //Enviamos la transaccion
+            const txid = await connection.sendRawTransaction(
+                transactionsignature.serialize()
+            );
+            console.info(`Transaccion con numero de id ${txid} enviada`);
+
+            //Esperamos a que se confirme la transaccion
+            const confirmation = await connection.confirmTransaction(txid, {
+                commitment: "singleGossip",
+            });
+
+            const { slot } = confirmation.value;
+
+            console.info(
+                `Transaccion con numero de id ${txid} confirmado en el bloque ${slot}`
+            );
+
+            const solanaExplorerLink = `https://explorer.solana.com/tx/${txid}?cluster=${SOLANA_NETWORK}`;
+            setExplorerLink(solanaExplorerLink);
+
+            toast.success("Transaccion enviada con exito :D ");
+
+            //Actualizamos el balance
+            getBalances(publicKey);
+            setAmount(null);
+            setReceiver(null);
+
+            return solanaExplorerLink;
+        } catch (error) {
+            console.error("ERROR SEND TRANSACTION", error);
+            toast.error("Error al enviar la transaccion");
+        }
+    };
+
+    //Función para subir archivos a IPFS
+
+    const { mutateAsync: upload } = useStorageUpload();
+
+    const uploadToIpfs = async (file) => {
+        setStatusText("Subiendo a IPFS...");
+        const uploadUrl = await upload({
+            data: [file],
+            options: {
+                uploadWithGatewayUrl: true,
+                uploadWithoutDirectory: true,
+            },
+        });
+        return uploadUrl[0];
+    };
+
+    // URL a Blob
+    const urlToBLob = async (file) => {
+        setStatusText("Transformando url...");
+        await fetch(url)
+            .then((res) => res.blob())
+            .then((myBlob) => {
+                // logs: Blob { size: 1024, type: "image/jpeg" }
+
+                myBlob.name = "blob.png";
+
+                file = new File([myBlob], "image.png", {
+                    type: myBlob.type,
+                });
+            });
+
+        const uploadUrl = await uploadToIpfs(file);
+        console.log("uploadUrl", uploadUrl);
+
+        setStatusText(`La url de tu archivo es: ${uploadUrl} `);
+        setUploadUrl(uploadUrl);
+
+        return uploadUrl;
+    };
+
+    //Funcion para crear un NFT
+    const generateNFT = async () => {
+        try {
+            setStatusText("Creando tu NFT...❤");
+            const mintedData = {
+                name: "Mi primer NFT con Superteam MX",
+                imageUrl: uploadUrl,
+                publicKey,
+            };
+            console.log("Este es el objeto mintedData:", mintedData);
+            setStatusText(
+                "Minteando tu NFT en la blockchain Solana 🚀 Porfavor espera..."
+            );
+            const { data } = await axios.post("/api/mintnft", mintedData);
+            const { signature: newSignature } = data;
+            const solanaExplorerUrl = `https://solscan.io/tx/${newSignature}?cluster=${SOLANA_NETWORK}`;
+            console.log("solanaExplorerUrl", solanaExplorerUrl);
+            setStatusText(
+                "¡Listo! Tu NFT se a creado, revisa tu Phantom Wallet 🖖"
+            );
+        } catch (error) {
+            console.error("ERROR GENERATE NFT", error);
+            toast.error("Error al generar el NFT");
+        }
+    };
+
+    const flag = async(index) =>{
+        sendTransaction();
+        console.log("img:",img[index].url);
+        console.log("index:",index);
+    }
     return (
-        <div>
-            <h1>Estamos en la pagina demo</h1>
+        <div className="h-screen bg-black">
+            <div className="flex flex-col  w-auto h-auto  bg-black">
+                <div className="flex flex-col py-24 place-items-center justify-center">
+                    <h1 className="text-5xl font-bold pb-10 text-emerald-200">
+                        kiosco
+                    </h1>
+
+                    {publicKey ? (
+                        <div className="flex flex-col py-14 place-items-center justify-center">
+                            <br />
+
+                            <h1 className="text-2xl font-bold text-white px-8">
+                                Tu numero de Nomina es <br/> {publicKey}<br />
+                            </h1>
+                            <br />
+
+                            <a href={explorerLink}>
+                                <h1 className="text-md font-bold text-sky-500">
+                                    {explorerLink}
+                                </h1>
+                            </a>
+                            <br />
+
+
+                            {
+                                explorerLink ? (
+                                   <div>
+                                    <p>ya paso la transaccion, pon imagen aqui</p>
+                                    <img src={img[0].url} />
+                                    </div>
+                                ):(
+                                    <br/>
+                                )
+                            }
+
+                            <br />
+
+                            <div className="appcontainer">
+                                <button position="bottom-center" className="inline-flex h-8 w-52 justify-center bg-purple-500 font-bold text-white" onClick={()=>flag(0)}>2023N23</button><br />
+                                <br/>
+                                <button position="bottom-center" className="inline-flex h-8 w-52 justify-center bg-purple-500 font-bold text-white" onClick={()=>flag(1)}>2023N22</button><br />
+                                <br/>
+                                <button position="bottom-center" className="inline-flex h-8 w-52 justify-center bg-purple-500 font-bold text-white" onClick={()=>flag(2)}>2023N21</button><br />
+                                <br/>
+                                <button position="bottom-center" className="inline-flex h-8 w-52 justify-center bg-purple-500 font-bold text-white" onClick={()=>flag(3)}>2023N20</button><br />
+                                <br/>
+                            </div>
+                            <br />
+                            <button
+                                type="submit"
+                                className="border-2 border-white px-5 bg-green-500"
+                                onClick={() => {
+                                    signOut();
+                                }}
+                            >
+                                Desconectar 📴
+                            </button>
+                        </div>
+                    ) : (
+                        <div className="flex flex-col place-items-center justify-center">
+                            <button
+                                type="submit"
+                                className="inline-flex h-8 w-52 justify-center bg-purple-500 font-bold text-white"
+                                onClick={() => {
+                                    signIn();
+                                }}
+                            >
+                                Ingresa a tu Nomina 💵
+                            </button>
+                            <img src="https://res.cloudinary.com/dceddepoe/image/upload/v1697189302/nominas/logo_pnw4uo.gif"></img>
+                        </div>
+                    )}
+                    
+                </div>
+                
+                <Toaster position="bottom-center" />
+            </div>
+
+            
+            
         </div>
+
+        
     );
-}
+};
+
+
+
+
+export default Home;
